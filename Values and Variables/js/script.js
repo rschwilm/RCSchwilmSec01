58a245ffaa9f563bc142c5a68543135a0bfe7170
Rebecca Schwilm <rcschwilm@fullsail.edu>
2014-10-06 18:06:15 -0400

    variables and values practice

---
+++ Values and Variables/js/script.js
@@ -0,0 +1,12 @@
+// JavaScript
+
+var muppetNames = ["Kermit", "Miss Piggy"];
+ //0, 1, 2, 3 are the index numbers within the array
+ 
+muppetNames[1] = "Rizzo"; //change index #1 to Rizzo
+console.log(muppetNames[1]); //lets me see what's in index #1
+muppetNames[2] = "Fozzy";
+console.log(muppetNames[2]); 
+
+
+
